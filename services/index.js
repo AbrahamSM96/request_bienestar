237c91a5fedb3e0d6ae1a70fe4c09a147e134d3a
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-02 14:06:47 -0600

    feat/add: feat update table, add fn delete supabase

--- services/index.js
+++ services/index.js
@@ -11,7 +11,35 @@ export const getUnities = async () => {
 };
 
 export const createTupleInMain = async (newTuple) => {
-  const { data, error } = await supabase.from("main").upsert([{ ...newTuple }]);
-  console.log({ data, error });
+  const { data, error } = await supabase
+    .from("main")
+    .insert([{ ...newTuple }])
+    .order("created_at", { ascending: false });
   return [error, data];
+};
+
+export const updateTupleInMain = async (id, updatedTask, tuples) => {
+  const findUpdatedTup = tuples.filter((tup) => tup.id_tuple === id);
+
+  const { data, error } = await supabase
+    .from("main")
+    .update(updatedTask)
+    .match(findUpdatedTup[0])
+    .order("created_at", { ascending: false });
+
+  const newDataUpdated = [
+    ...tuples.map((tup) =>
+      tup.id_tuple === id ? { ...tup, ...updatedTask } : tup,
+    ),
+  ];
+  return [error, newDataUpdated];
+};
+
+export const deleteTupleMain = async (id, tuples) => {
+  const { error, data } = await supabase
+    .from("main")
+    .delete()
+    .match({ id_tuple: id });
+  const newData = [...tuples.filter((tup) => tup.id_tuple !== id)];
+  return [error, newData];
 };
