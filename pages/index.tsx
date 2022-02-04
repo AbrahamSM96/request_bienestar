79c59c3be40ad9801ccd87a8d9c6d87fe50ad394
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-03 23:52:46 -0600

    add: add component Spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Table from "../components/Table";
 import { useTuples } from "../context/tuplesContext";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import Spinner from "../components/Spinner/Spinner";
 
 const Home: NextPage = () => {
   const { tuples } = useTuples();
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
     <>
       <Layout>
         {tuples.length === 0 ? (
-          <h2>There are no request</h2>
+          <Spinner />
         ) : (
           <>
             <Table />
