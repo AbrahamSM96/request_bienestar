754b4ff33cde47b9c562f4e9be997fb0d9452cc2
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-03 23:51:26 -0600

    add: added spinner to load table

---
+++ components/Spinner/Spinner.tsx
@@ -0,0 +1,19 @@
+import styles from "./Spinner.module.css";
+
+export default function Spinner() {
+  return (
+    <div className="h-screen bg-morenaBold">
+      <div className={styles.skCubeGrid}>
+        <div className={`${styles.skCube} ${styles.skCube1}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube2}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube3}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube4}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube5}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube6}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube7}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube8}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube9}`}></div>
+      </div>
+    </div>
+  );
+}
