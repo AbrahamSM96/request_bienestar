1a1d2ad474f04e98b06bf5a4502ea6e609673ca0
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-17 01:27:04 -0600

    fix(instance): create only one instance and evit many prisma instances

--- pages/api/postRequerimientos.ts
+++ pages/api/postRequerimientos.ts
@@ -1,11 +1,31 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "lib/prisma";
+const crypto = require("crypto");
 
 export default async (req, res) => {
+  // post
+  const {
+    unities,
+    quantities: unidad,
+    classification: clasificacion,
+    items: item,
+    notes: nota,
+  } = req.body;
+
+  const id = crypto.randomUUID();
+  const cantidad = parseInt(unities);
+
   try {
-    // post
-    const data = await prisma.requerimientos.findMany();
+    const data = await prisma.requerimientos.create({
+      data: {
+        id,
+        cantidad,
+        unidad,
+        clasificacion,
+        item,
+        nota,
+      },
+    });
+
     return res.status(200).send(data);
   } catch (error) {
     return res.status(500).send({ error });
