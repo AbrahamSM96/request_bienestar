19c29542f1f854809c58234160a8bc3f38b9b2d4
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-18 02:07:19 -0600

    feat(rendertable): delete orderby in the front

--- components/Tbody.tsx
+++ components/Tbody.tsx
@@ -5,19 +5,11 @@ import Tr from "./Tr";
 
 export default function Tbody() {
   const { tuples } = useTuples();
-  tuples.sort((tupA, tupB) =>
-    tupA.id > tupB.id ? 1 : tupA.id < tupB.id ? -1 : 0,
-  );
-
   return (
     <tbody>
       {tuples.map((tuple: any, index: Number) => {
         return (
-          <Tr
-            key={`tuple-${tuple.id_tuple}-${index}`}
-            {...tuple}
-            index={index}
-          />
+          <Tr key={`tuple-${tuple.id}-${index}`} {...tuple} index={index} />
         );
       })}
     </tbody>
